e3d97ec5bf278afbb83b6f4d1cc504e1581fd152
Catavei <RO100162@Main.intgin.net>
2020-11-11 21:30:31 +0200

    Signed-off-by: Catavei <RO100162@Main.intgin.net>

--- EEG.py
+++ EEG.py
@@ -1,28 +1,15 @@
-NO_OF_MFCCS = 18
-NO_OF_CHANNELS = 62
 
 class Channel:
     def __init__(self, channel_no, channel_label, MFCCs):
         self.channel_no = channel_no
         self.channel_label = channel_label
-        self.MFFCs = MFCCs
+        self.MFCCs = MFCCs
         
 class EEG:
-    def __init__(self, input_table):
+    def __init__(self, subject, class_nr, class_label, channels):
         
-        self.subject = str(input_table[0, 0])
-        self.class_nr = int(input_table[0, 2])
-        self.class_label = str(input_table[0, 3])
-        self.channels = []
+        self.subject = subject
+        self.class_nr = class_nr
+        self.class_label = class_label
+        self.channels = channels
         
-        for i in range(NO_OF_CHANNELS):
-            
-            channel_label = str(input_table[i,5])
-            MFCCs = input_table[i, 11:]
-            MFCC_list = []
-            
-            for j in range(NO_OF_MFCCS):
-                MFCC_list.append(float(MFCCs[j]))
-                
-            channel = Channel(i, channel_label, MFCC_list)
-            self.channels.append(channel)
--- main.py
+++ main.py
@@ -1,21 +1,115 @@
 import csv
+import statistics 
 import numpy as np
-from EEG import EEG
+import matplotlib.pyplot as plt
+from EEG import EEG, Channel
 
+
+NO_OF_MFCCS = 18
+NO_OF_CHANNELS = 62
+NO_OF_EEGS = 993
+NO_OF_GAUSSIAN_EEGS = int(0.3*NO_OF_EEGS)
+
+
+def create_initial_EEGs(raw_input):
+    data_in = raw_input[1:, 1:30]
+
+    tables_array = np.split(data_in, NO_OF_EEGS)
+    
+    EEGs = []
+    
+    for table in tables_array:
+        
+        subject = str(table[0, 0])
+        class_nr = int(table[0, 2])
+        class_label = str(table[0, 3])
+        channels = []
+        
+        for i in range(NO_OF_CHANNELS):
+            
+            channel_label = str(table[i,5])
+            MFCCs = table[i, 11:]
+            MFCC_list = []
+            
+            for j in range(NO_OF_MFCCS):
+                MFCC_list.append(float(MFCCs[j]))
+                
+            channel = Channel(i, channel_label, MFCC_list)
+            channels.append(channel)
+        
+        EEGs.append(EEG(subject, class_nr, class_label, channels))
+    
+    return EEGs
+
+
+def get_gaussian_params(MFCC_tables):
+    
+    MFCC_tables = np.array(MFCC_tables)
+    mus = np.zeros((NO_OF_CHANNELS, NO_OF_MFCCS))
+    sigmas = np.zeros((NO_OF_CHANNELS, NO_OF_MFCCS))
+    
+    for i in range(NO_OF_CHANNELS):
+        for j in range(NO_OF_MFCCS):
+            mus[i][j] = sum(MFCC_tables[:,i,j])/NO_OF_EEGS
+            sigmas[i][j] = statistics.stdev(MFCC_tables[:,i,j])
+    
+    return mus, sigmas
+
+
+def get_MFCC_tables(EEGs):
+    
+    MFCC_tables = []
+    
+    for EEGx in EEGs:
+        
+        MFCC_table = []
+        
+        for channel in EEGx.channels:
+            MFCC_table.append(channel.MFCCs)
+            
+        MFCC_tables.append(MFCC_table)
+    
+    return MFCC_tables
+
+
+def generate_gaussian_EEGS(mus, sigmas):
+    
+    generated_EEGs = np.zeros((NO_OF_GAUSSIAN_EEGS, NO_OF_CHANNELS, NO_OF_MFCCS))
+    
+    for i in range(NO_OF_CHANNELS):
+        for j in range(NO_OF_MFCCS):
+            generated_EEGs[:,i,j] = np.random.normal(mus[i,j], sigmas[i,j], NO_OF_GAUSSIAN_EEGS)
+            
+            
+    return generated_EEGs
+    
+    
 reader = csv.reader(open("input_data.csv"), delimiter=",")
-
 raw_input = np.array(list(reader))
-data_in = raw_input[1:, 1:30]
 
-tables_array = np.split(data_in, data_in.shape[0]/62)
 
-EEGs = []
+EEGs = create_initial_EEGs(raw_input)
 
-for table in tables_array:
-    EEGs.append(EEG(table))
+MFCC_tables = get_MFCC_tables(EEGs)
+MFCC_tables = np.array(MFCC_tables)
+
+mus, sigmas = get_gaussian_params(MFCC_tables)
+        
+gaussian_EEGs = generate_gaussian_EEGS(mus, sigmas)
+
+
+
+x = list(MFCC_tables[:,0,0])
+
+hist = np.histogram(x, bins=100)
+plt.plot(hist[0])
+plt.show()
+
+y = list(gaussian_EEGs[:,0,0])
+hist = np.histogram(y, bins = 100)
+plt.plot(hist[0])
+plt.show()
     
-del tables_array, table, reader, data_in, raw_input
 
+del reader
 
-
-    
